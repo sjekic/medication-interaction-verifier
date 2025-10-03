d7746f943d23006ed27349461188dcbf85202b5d
Simonida Jekic <sjekic.ieu2023@student.ie.edu>
2025-10-03 17:23:44 +0200

    finish UI and add a page dedicated to finding pharmacies nearby

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500"
-    ],   # addresses your VS Code Live Server uses
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
